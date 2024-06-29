c5b84a5851c8949bbed755f3647f56f4fbd40e8d
Daniel Fortuyn <daniel@frtn.nl>
2024-06-29 15:47:35 +0200

    init

---
+++ types/package.d.ts
@@ -0,0 +1,5 @@
+export type Package = {
+    id: string;
+    label: string;
+    bytes: number;
+  };
---
+++ types/partner.d.ts
@@ -0,0 +1,20 @@
+export type Partner = {
+    name: string;
+    pricingStrategy: PricingStrategy;
+    packageStrategy: PackageStrategy;
+}
+
+export type PricingStrategy = {
+}
+
+export type PackageStrategy = {
+
+}
+
+export type IframeConfig = {
+
+}
+
+export type TravelSpiritConfig = {
+
+}
