0c7be1179bd397c69d1d2bebd29fba25e496ce4d
Stefan Cekov <stefancekov@Stefans-MacBook-Pro.local>
2024-08-20 13:21:36 +0200

    Added contry type

---
+++ types/country.d.ts
@@ -0,0 +1,12 @@
+export type Country = {
+    lte: boolean | null; // Indicates if LTE is enabled
+    apn: string | null; // Access Point Name, e.g., "globaldata"
+    click_count: number | null; // Number of times the network was clicked
+    global_network: string | null; // Name of the global network, e.g., "SETAR GSM"
+    global_price: number | null; // Global price associated with the network
+    hubby: number | null; // An associated value, possibly a constant or identifier
+    imsi: number | null; // International Mobile Subscriber Identity
+    name: string | null; // Name of the network, e.g., "Aruba"
+    tier: number | null; // Indicates the tier level, e.g., 2
+  };
+  
