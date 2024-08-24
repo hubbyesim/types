3bc206f1cf00b452883ee82316123b21472c2a6f
Daniel Fortuyn <daniel@frtn.nl>
2024-08-24 13:35:30 +0200

    extra type work

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,6 @@ export * from './traveler';
 export * from './package';
 export * from './partner';
 export * from './user';
-
+export * from './country';
+export * from './booking';
+export * from './promo_code';   
