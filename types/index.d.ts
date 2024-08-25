1f5710469e1f9b0256a5dd6882cca357beee1c83
Daniel Fortuyn <daniel@frtn.nl>
2024-08-25 18:21:24 +0200

    promocode fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,4 @@ export * from './partner';
 export * from './user';
 export * from './country';
 export * from './booking';
-export * from './promo_code';
+export * from './promoCode';
--- types/promoCode.d.ts
+++ types/promoCode.d.ts
@@ -1,16 +1,20 @@
-import { DocumentReference } from '@google-cloud/firestore';
-import { Booking } from './booking';
+import { DocumentReference } from "firebase-admin/firestore";
+import { Timestamp } from "firebase-admin/firestore";
+import { Partner } from "./partner";
+import { Country } from "./country";
 
 export type PromoCode = {
-  allowance_total: number | null; // Total users that can use the promo code
-  allowance_user: number | null; // Total number of times a user can reuse the promo code
-  country: string | null; // Reference to a Country document
-  external_id: string | null; // External identifier for the promo codes, comes from the client side
-  package: string | null; // Reference to a Package document
-  partner: string | null; // Reference to a Partner document
-  usage: string[] | null; // Array of user IDs that used the promo code
-  valid_from: Date | null; // Date when the promo code becomes valid
-  valid_to: Date | null; // Date when the promo code expires
-  updated_at: Date | null; // Date when the promo code was last updated
-  booking: DocumentReference<Booking> | null; // Reference to a Booking document
-};
+  id: string;
+  code: string;
+  discount: number;
+  allowance_total: number;
+  allowance_user: number
+  partner: DocumentReference<Partner>;
+  country: DocumentReference<Country>;
+  valid_from: string | Date | Timestamp;
+  valid_to: string | Date | Timestamp;
+  created_at: string | Date | Timestamp;
+  updated_at: string | Date | Timestamp;
+  created_by: string;
+  updated_by: string;
+}
