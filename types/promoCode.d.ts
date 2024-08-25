3524a1e01ab3c80f6f5a9ad22eecab6ddc842cb1
Daniel Fortuyn <daniel@frtn.nl>
2024-08-25 18:27:44 +0200

    promo code updates

--- types/promoCode.d.ts
+++ types/promoCode.d.ts
@@ -2,19 +2,21 @@ import { DocumentReference } from "firebase-admin/firestore";
 import { Timestamp } from "firebase-admin/firestore";
 import { Partner } from "./partner";
 import { Country } from "./country";
+import { Booking } from "./booking";
+import { HubbyModel } from "./hubby";
 
-export type PromoCode = {
+export type HubbyPromoCode = {
   id: string;
   code: string;
   discount: number;
   allowance_total: number;
-  allowance_user: number
+  allowance_user: number;
+  package_size: string;
   partner: DocumentReference<Partner>;
   country: DocumentReference<Country>;
   valid_from: string | Date | Timestamp;
   valid_to: string | Date | Timestamp;
-  created_at: string | Date | Timestamp;
-  updated_at: string | Date | Timestamp;
-  created_by: string;
-  updated_by: string;
+  booking: DocumentReference<Booking> | null;
 }
+
+export type PromoCode = HubbyPromoCode & HubbyModel;
