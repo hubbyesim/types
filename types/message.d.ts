074ecfe2a27c868de9b6e17aa05212619ffd3537
Daniel Fortuyn <daniel@frtn.nl>
2024-08-28 15:10:18 +0200

    minor changes

--- types/message.d.ts
+++ types/message.d.ts
@@ -1,8 +1,12 @@
+import { Timestamp } from "firebase-admin/firestore";
+
 export type Message = {
     id: string;
     key: string;
     method: "sms" | "email" | "push";
     status: "pending" | "sent" | "failed" | "delivered";
+    created_at: Timestamp
+    updated_at: Timestamp
   };
   
   export type SentMessages = {
