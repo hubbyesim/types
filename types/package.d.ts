d1f81bb976f1fe7018d41022a7facc3be7ac8e52
Daniel Fortuyn <daniel@frtn.nl>
2024-08-30 16:16:31 +0200

    v1.1.15 country is docref

--- types/package.d.ts
+++ types/package.d.ts
@@ -1,10 +1,11 @@
+import { DocumentReference } from "firebase-admin/firestore";
 import { Country } from "./country";
 
 export type Package = {
   id: string;
   label: string;
   bytes: number;
-  country: Country | undefined | null;
+  country: DocumentReference<Country>;
   hidden: boolean;
   price: number;
   partner_price: number;
