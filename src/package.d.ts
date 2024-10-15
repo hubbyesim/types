769e5dc15a2e409ad04ca0bd7d4cf7cf73a71009
Stefan Cekov <stefancekov@Stefans-MacBook-Pro.local>
2024-10-15 14:08:11 +0200

    added price modification property in package

--- src/package.d.ts
+++ src/package.d.ts
@@ -1,14 +1,25 @@
 import { DocumentReference } from "firebase-admin/firestore";
 import { Country } from "./country";
 import { HubbyModel } from "./hubby";
+import { Partner } from "./partner";
 
 export type Package = {
-  id: string;
   label: string;
   bytes: number;
-  country: DocumentReference<Country>;
+  size_in_gb: number;
+  country: DocumentReference<Country>; //Hrvatska
   hidden: boolean;
   price: number;
   partner_price: number;
   days: number;
+  price_modifications: PriceModifications;
 } & HubbyModel
+
+type PriceModifications = [PriceModification]
+
+type PriceModification = {
+  partner: DocumentReference<Partner>; //Hubby
+  fixed_price: number | null;
+  fixed_percentage: number | null;
+  price_type: 'fixed' | 'percentage';
+}
