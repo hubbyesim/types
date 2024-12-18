095f6a77c797f23e13148385c9e776ceefae6088
Stefan <stefan@hubbyesim.com>
2024-12-18 10:49:46 +0100

    PromoCode model refactored a bit, added optional fields based on type

--- src/promoCode.d.ts
+++ src/promoCode.d.ts
@@ -7,23 +7,25 @@ import { HubbyModel } from "./hubby";
 import { Package } from "./package";
 
 export type HubbyPromoCode = {
-  code: string;
-  discount: number;
-  allowance_total: number;
-  allowance_user: number;
-  package_size: string;
-  package: DocumentReference<Package>;
-  partner: DocumentReference<Partner>;
-  country: DocumentReference<Country>;
-  valid_from: string | Date | Timestamp;
-  valid_to: string | Date | Timestamp;
-  booking: DocumentReference<Booking> | null;
   external_id: string;
-  type?: 'full-discount' | 'partial-discount' | 'booking' | 'traveler' | 'referral' | null | string;
+  code: string;
+  allowance_user: number;
+  allowance_total: number;
+  type: 'full-discount' | 'partial-discount' | 'booking' | 'traveler' | null | string;
+  usage: string[],
+  uuid_usage: string[],
+
+  //Optional fields based on the type
+  discount?: number;
+  package_size?: string;
+  package?: DocumentReference<Package>;
+  partner?: DocumentReference<Partner>;
+  country?: DocumentReference<Country>;
+  valid_from?: string | Date | Timestamp;
+  valid_to?: string | Date | Timestamp;
+  booking?: DocumentReference<Booking> | null;
   countries?: string[];
   max_bytes?: number;
-  usage?: string[],
-  uuid_usage?: string[],
   starter_data?: number
 }
 
