b5721657f85b2c4338b5a0b2fc61ec29107d469e
Daniel Fortuyn <daniel@frtn.nl>
2025-05-14 20:52:27 +0200

    deduplication

--- src/specs/partner.ts
+++ src/specs/partner.ts
@@ -166,6 +166,8 @@ export const freeEsimSchema = z.object({
         type: z.string(),
         destination: z.string()
     }),
+    booking_id_verification: z.boolean().default(false),
+    booking_id_verification_pattern: z.string().nullable().optional(),
     allowance: z.number()
 });
 
@@ -269,15 +271,7 @@ export const platformSettingsSchemaSpec = markAsSchemaSpec({
     },
     free_esim: {
         _type: 'object' as const,
-        of: {
-            package_specification: {
-                _type: 'object' as const,
-                of: packageSpecificationSchema.shape
-            },
-            booking_id_verification: z.boolean().nullable().optional(), //This could be a regex pattern to test the booking id
-            booking_id_verification_pattern: z.string().nullable().optional(), //This could be a regex pattern to test the booking id
-            allowance: z.number()
-        },
+        of: freeEsimSchema.shape,
         nullable: true,
         optional: true
     },
