5c3dcf2b4a91277a484ce8dd4d46f456dd14643e
Daniel Fortuyn <daniel@frtn.nl>
2025-05-19 07:58:01 +0200

    small fix

--- src/specs/analytics.ts
+++ src/specs/analytics.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 import { markAsSchemaSpec } from '../common';
-import { hubbyModelSpec } from './common';
+import { hubbyModelSpec, PARTNER_COLLECTION } from './common';
 
 // Country schema spec
 export const analyticsSpec = markAsSchemaSpec({
     ...hubbyModelSpec,
     service: z.string(),
     date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/), // YYYY-MM-DD
-    partner: z.string(),
+    partner: { _type: 'docRef' as const, collection: PARTNER_COLLECTION, nullable: true },
     event: z.string(),
     sum: z.number(),
 });
